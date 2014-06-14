a1e1488b4e8a48546bf597bd46dcd3760227a2b7
TheDarkVoid <guyana393@gmail.com>
2014-06-14 13:23:16 -0400

    Improved Accuracy of Coin/Alien Collision

--- js/game/ObjectClasses/Coin.js
+++ js/game/ObjectClasses/Coin.js
@@ -32,14 +32,29 @@ Coin.prototype = {
 
 	DetectCollisions : function(event)
 	{
-		var playerBounds = this.mGame.GetPlayer().getBounds();
-		var coinBounds = this.getBounds();
-		if (coinBounds.intersects(playerBounds, 0.7, 0.7)) 
+		var player = this.mGame.GetPlayer();
+		var rect2 = new TGE.Rectangle(player.worldX - player.width/2, player.worldY - player.height/2, player.width, player.height);
+		var rect1 = new TGE.Rectangle(this.worldX - this.width/2, this.worldY - this.height/2, this.width, this.height);
+		//console.log(this.width);
+
+		var ret = false;
+		var r1 = rect1.x + rect1.width;
+		var r2 = rect2.x + rect2.width;
+		if((rect1.x >= rect2.x && rect1.x <= r2) || (r1 >= rect2.x && r1 <= r2))
+		{
+			var y1 = rect1.y + rect1.height;
+			var y2 = rect2.y + rect2.height;
+			if((rect1.y <= y2 && rect1.y >= rect2.y) || (y1 <= y2 && y1 >= rect2.y))
+			{
+				ret = true;
+			}
+		}
+		if (ret) 
 		{
 			if(this.mIsAlien)
 			{
-				//this.mGame.GetPlayer().mStopped = true;
-				//this.mGame.PlayerHitObstacle("alien");
+				this.mGame.GetPlayer().mStopped = true;
+				this.mGame.PlayerHitObstacle("alien");
 				this.markForRemoval();	
 				// //Play background music
 				// TGE.Game.GetInstance().audioManager.Play({ 
